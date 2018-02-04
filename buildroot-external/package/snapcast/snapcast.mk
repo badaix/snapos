7ce89347c526186d1c97c16a183f0d99fed7c0f9
badaix <johannes.pohl@badaix.de>
2018-02-04 13:59:42 +0100

    add missing dependencies for Snapcast

--- buildroot-external/package/snapcast/snapcast.mk
+++ buildroot-external/package/snapcast/snapcast.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SNAPCAST_VERSION = 39b880e6
+SNAPCAST_VERSION = fc075555
 SNAPCAST_SITE = $(call github,badaix,snapcast,$(SNAPCAST_VERSION))
 SNAPCAST_LICENSE = GPL-3.0+
 SNAPCAST_LICENSE_FILES = copying.txt
