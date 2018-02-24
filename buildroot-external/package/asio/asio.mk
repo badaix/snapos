f0e6811ba1d29c3193d6098f406df65eef6f2efa
badaix <johannes.pohl@badaix.de>
2018-02-24 10:29:29 +0100

    use autotools to install asio

--- buildroot-external/package/asio/asio.mk
+++ buildroot-external/package/asio/asio.mk
@@ -4,14 +4,37 @@
 #
 ################################################################################
 
-ASIO_VERSION = 28d9b8d6df708024af5227c551673fdb2519f5bf
+ASIO_VERSION = asio-1-10-8
 ASIO_SITE = $(call github,chriskohlhoff,asio,$(ASIO_VERSION))
-ASIO_LICENSE = Boost Software License
-ASIO_LICENSE_FILES = LICENSE_1_0.txt
+ASIO_LICENSE = Boost Software License 1.0
+ASIO_LICENSE_FILES = COPYING, LICENSE_1_0.txt
 
-# asio is a header-only library, it only makes sense
-# to have it installed into the staging directory.
-ASIO_INSTALL_STAGING = YES
+ASIO_SUBDIR = asio
+
+# patching configure.ac and Makefile.am
+ASIO_AUTORECONF = YES
+
+ASIO_CONF_OPTS = --disable-examples --disable-tests
+
+# Only installs headers
 ASIO_INSTALL_TARGET = NO
+ASIO_INSTALL_STAGING = YES
 
-$(eval $(cmake-package))
+ifeq ($(BR2_PACKAGE_BOOST),y)
+ASIO_DEPENDENCIES += boost
+ASIO_CONF_OPTS += --with-boost=yes
+ifeq ($(BR2_PACKAGE_BOOST_COROUTINE),y)
+ASIO_CONF_OPTS += --enable-boost-coroutine
+endif
+else
+ASIO_CONF_OPTS += --with-boost=no
+endif
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+ASIO_DEPENDENCIES += openssl
+ASIO_CONF_OPTS += --with-openssl=yes
+else
+ASIO_CONF_OPTS += --with-openssl=no
+endif
+
+$(eval $(autotools-package))
