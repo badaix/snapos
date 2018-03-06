1a716bd8c9e26e56bd5337913ac487e6dcc3c811
badaix <johannes.pohl@badaix.de>
2018-03-06 18:08:47 +0100

    use post_install hook for config installation

--- buildroot-external/package/snapcast/snapcast.mk
+++ buildroot-external/package/snapcast/snapcast.mk
@@ -10,20 +10,26 @@ SNAPCAST_DEPENDENCIES = libogg alsa-lib avahi # libstdcpp libatomic libflac libv
 SNAPCAST_LICENSE = GPL-3.0+
 SNAPCAST_LICENSE_FILES = LICENSE
 
-
-ifeq ($(BR2_PACKAGE_SNAPCAST_CLIENT),y)
 define SNAPCLIENT_INSTALL_CONFIG
 	mkdir -p $(TARGET_DIR)/etc/default
 	$(INSTALL) -m 0755 -D $(@D)/client/debian/snapclient.default $(TARGET_DIR)/etc/default/snapclient
 endef
 
+
+define SNAPSERVER_INSTALL_CONFIG
+	mkdir -p $(TARGET_DIR)/etc/default
+	$(INSTALL) -m 0755 -D $(@D)/server/debian/snapserver.default $(TARGET_DIR)/etc/default/snapserver
+endef
+
+
+ifeq ($(BR2_PACKAGE_SNAPCAST_CLIENT),y)
+SNAPCAST_POST_INSTALL_TARGET_HOOKS += SNAPCLIENT_INSTALL_CONFIG
+
 define SNAPCLIENT_INSTALL_INIT_SYSV
-	$(SNAPCLIENT_INSTALL_CONFIG)
 	$(INSTALL) -m 0755 -D $(SNAPCAST_PKGDIR)/S99snapclient $(TARGET_DIR)/etc/init.d/S99snapclient
 endef
 
 define SNAPCLIENT_INSTALL_INIT_SYSTEMD
-	$(SNAPCLIENT_INSTALL_CONFIG)
 	$(INSTALL) -D -m 0644 $(@D)/client/debian/snapclient.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/snapclient.service
 
@@ -36,18 +42,13 @@ endif
 
 
 ifeq ($(BR2_PACKAGE_SNAPCAST_SERVER),y)
-define SNAPSERVER_INSTALL_CONFIG
-	mkdir -p $(TARGET_DIR)/etc/default
-	$(INSTALL) -m 0755 -D $(@D)/server/debian/snapserver.default $(TARGET_DIR)/etc/default/snapserver
-endef
+SNAPCAST_POST_INSTALL_TARGET_HOOKS += SNAPSERVER_INSTALL_CONFIG
 
 define SNAPSERVER_INSTALL_INIT_SYSV
-	$(SNAPSERVER_INSTALL_CONFIG)
 	$(INSTALL) -m 0755 -D $(SNAPCAST_PKGDIR)/S99snapserver $(TARGET_DIR)/etc/init.d/S99snapserver
 endef
 
 define SNAPSERVER_INSTALL_INIT_SYSTEMD
-	$(SNAPSERVER_INSTALL_CONFIG)
 	$(INSTALL) -D -m 0644 $(@D)/server/debian/snapserver.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/snapserver.service
 
